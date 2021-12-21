dc258b4dc4dadd4d4c42e44f5926246386d80a56
gr-kim-94 <muuulll@jj.ac.kr>
2021-12-22 00:22:58 +0900

    Second Week - Number Line Jumps 임시커밋

--- HackerRank/Algorithms/NumberLineJumps.playground/Contents.swift
+++ HackerRank/Algorithms/NumberLineJumps.playground/Contents.swift
@@ -1,6 +1,16 @@
 import Cocoa
 
 func kangaroo(x1: Int, v1: Int, x2: Int, v2: Int) -> String {
-    // Write your code here
-
+    let x = Double(x2 - x1) / Double(v1 - v2)
+    print(x)
+    let isInteger = floor(x) == x
+    return (x > 0 && isInteger) ? "YES" : "NO"
 }
+
+kangaroo(x1: 0, v1: 3, x2: 4, v2: 2)
+// 0 + 3s = 4 + 2s
+// (3 - 2)s = 4 - 0
+// s = (4 - 0)/(3 - 2)
+// YES
+
+kangaroo(x1: 0, v1: 2, x2: 5, v2: 3)
