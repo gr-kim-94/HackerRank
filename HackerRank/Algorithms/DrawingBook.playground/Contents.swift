e5ac92d57eb6e71dfe3e3bd37b405fb78b081ad4
gr-kim-94 <muuulll@jj.ac.kr>
2022-01-08 01:52:33 +0900

    Fifth Week - Algorithms / Drawing Book

--- HackerRank/Algorithms/DrawingBook.playground/Contents.swift
+++ HackerRank/Algorithms/DrawingBook.playground/Contents.swift
@@ -5,19 +5,27 @@ func pageCount(n: Int, p: Int) -> Int {
         // 첫페이지나 마지막페이지일 경우
         return 0
     }
-    
-    if n%2 == 0 {
-        // n = 6 : 1, 2 3, 4 5, 6
-        // n = 10 : 1, 2 3, 4 5, 6 7, 8 9, 10
-    } else {
-        // n = 7 : 1, 2 3, 4 5, 6 7
-        // n = 9 : 1, 2 3, 4 5, 6 7, 8 9
+    let page = p/2
+    return min(n/2 - page, page)
+}
+
+// pageCount RuntimeError -> pageCount2 Success
+func pageCount2(n: Int, p: Int) -> Int {
+    if p == 1 || p == n {
+        return 0
+    }
+    let lastPage = n/2 - p/2
+    if lastPage < p/2 {
+        return lastPage
     }
     
-    return 0
+    return p/2pageCount(n: 5, p: 4)
 }
 
 pageCount(n: 5, p: 3)
-pageCount(n: 11, p: 2)
-pageCount(n: 10, p: 2)
-pageCount(n: 10, p: 8)
+pageCount(n: 5, p: 2)
+pageCount(n: 5, p: 1)
+pageCount(n: 6, p: 5)
+pageCount(n: 6, p: 4)
+pageCount(n: 6, p: 3)
+pageCount(n: 6, p: 2)
