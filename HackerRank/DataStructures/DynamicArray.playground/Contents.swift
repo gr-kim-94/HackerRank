13fb404e58d46649b6084187d3e776124813ea5e
gr-kim-94 <muuulll@jj.ac.kr>
2022-01-25 12:04:14 +0900

    Seventh Week (임시커밋) - DataStructures/Dynamic Array (Time out)

--- HackerRank/DataStructures/DynamicArray.playground/Contents.swift
+++ HackerRank/DataStructures/DynamicArray.playground/Contents.swift
@@ -1,6 +1,6 @@
 import Cocoa
 
-func dynamicArray(n: Int, queries: [[Int]]) -> [Int] {
+func dynamicArray1(n: Int, queries: [[Int]]) -> [Int] {
     var arr:[[Int]] = [[],[]]
     var lastAnswer:[Int] = []
     queries.forEach { querie in
@@ -20,12 +20,36 @@ func dynamicArray(n: Int, queries: [[Int]]) -> [Int] {
             arr[idx].append(querie[2])
         } else {
             // querie[0] == 2
-            let dd = arr[idx][querie[2]%arr[idx].count]
-            lastAnswer.append(dd)
+            lastAnswer.append(arr[idx][querie[2]%arr[idx].count])
         }
     }
     
     return lastAnswer
 }
+
+// 가변 데이터 n 적용, arr에서 last 찾지않고 lastAnswer 변수로 저장
+func dynamicArray(n: Int, queries: [[Int]]) -> [Int] {
+    var arr = Array(repeating: [Int](), count: n)
+    var list = [Int]()
+    var lastAnswer = 0
+    
+//    for querie in queries {
+    queries.forEach { querie in
+        let x = querie[1]
+        let y = querie[2]
+        let idx = (x ^ lastAnswer) % n
+        
+        if querie[0] == 1 {
+            arr[idx].append(y)
+        } else {
+            let arrIdx = arr[idx]
+            lastAnswer = arrIdx[y%(arrIdx.count)]
+            list.append(lastAnswer)
+        }
+    }
+
+    return list
+}
+
 
 dynamicArray(n: 2, queries: [[1,0,5],[1,1,7],[1,0,3],[2,1,0],[2,1,1]])
